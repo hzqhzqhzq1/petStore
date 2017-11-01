365f01ce18856996e3919bdc283249d1b5a08930
hzqhzqhzq <15111468543@163.com>
2017-11-01 20:54:49 +0800

    第一次提交丢失，重新设计实体类

--- src/com/newer/petstore/domain/Category.java
+++ src/com/newer/petstore/domain/Category.java
@@ -1,5 +1,8 @@
 package com.newer.petstore.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 分类（名称、很多商品）
  * 
@@ -7,5 +10,44 @@ package com.newer.petstore.domain;
  *
  */
 public class Category {
+	private int id;
+	// 分类的名称
+	private String title;
 
+	// 该分类下的所有商品
+	private List<Product> productList = new LinkedList<Product>();
+
+	public Category() {
+		super();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Product> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+	/**
+	 * 在分类中添加商品
+	 */
+	public void addProduct(Product pro) {
+		productList.add(pro);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
